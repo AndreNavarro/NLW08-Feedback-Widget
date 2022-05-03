f9473ee2e12e49e2251f316180569981d20ea24a
Andre <andre-navarro@hotmail.com>
2022-05-02 21:34:55 -0300

    :tada: Start Feedback-Widget project with Tailwind

---
+++ web/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+interface ButtonProps {
+  text?: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button className='bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors'>{props.text ?? 'Default'}</button>
+}
+
+
+function App() {
+  return (
+    <div className='flex gap-2'>
+      <Button text="Enviar" />
+      <Button text="OK" />
+      <Button />
+    </div>
+  )
+}
+
+export default App
